b84b708d7cc641f8fc09b68e5e29f076fe4a0f0a
坏掉的世界 <459770648@qq.com>
2023-05-16 14:32:49 +0800

    实现基本列表内容的展示

--- app01/views.py
+++ app01/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import NavBar
+from .models import NavBar,Post
 # Create your views here.
 
 # 获取Nav_bar
 def view(request):
-    nav_bar_list = NavBar.objects.all()  # 查询所有用户
-    for item in nav_bar_list:
-        print(item.name,item.url)
-    return render(request,"index.html",{"nav_bar_list":nav_bar_list})
+    # 查询所有导航栏分类
+    nav_bar_list = NavBar.objects.all()
+    # 查询发布的文章
+    article_list = Post.objects.all()
+    print(article_list[0].url)
+    return render(request,"index2.html",{"nav_bar_list":nav_bar_list,"article_list":article_list})
